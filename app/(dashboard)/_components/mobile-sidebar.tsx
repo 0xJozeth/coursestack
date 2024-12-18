42a4a935a5f547133fdc0e44bea5d91a8b063adf
0xJozeth <0xJozeth@gmail.com>
2024-12-17 21:40:25 -0500

    update navigation, routing and dependencies

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,5 +1,19 @@
 import { Menu } from "lucide-react";
 
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
+import { DialogTitle } from "@radix-ui/react-dialog";
+
 export const MobileSidebar = () => {
-  return <Menu />;
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent position="left" className="p-0 bg-white">
+        <DialogTitle className="hidden">Mobile Sidebar</DialogTitle>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 };
